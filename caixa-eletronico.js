434dc7b65423fe92edfbe5dd1c998f525d253ace
joaoemanuels <jemanuel.pi@gmail.com>
2025-10-30 20:54:33 -0300

    Simulador de saque feito

---
+++ caixa-eletronico.js
@@ -0,0 +1,28 @@
+//Simulador de Caixa Eletrônico
+
+const simularSaque = (valorSaque, saldo) => {
+  let notas = [50, 20, 10, 5];
+  let valorTotal = valorSaque;
+  let qtdNotas = [];
+  let agora = new Date();
+  let hora = agora.getHours();
+  let minutos = agora.getMinutes();
+
+  if (valorSaque % 5 === 0 && saldo >= valorSaque) {
+    for (let cedula of notas) {
+      let qtd = Math.floor(valorSaque / cedula);
+      qtdNotas.push(qtd);
+      valorSaque -= qtd * cedula;
+    }
+  } else {
+    console.log("Valor inválido, tente outro");
+  }
+  console.log(`Saque de ${valorTotal} realizado com sucesso às ${hora}:${minutos}!
+    |   Extrato   |
+    ${qtdNotas[0]} notas de R$50
+    ${qtdNotas[1]} notas de R$20
+    ${qtdNotas[2]} notas de R$10
+    ${qtdNotas[3]} notas de R$5`);
+};
+
+simularSaque(315, 1000);
