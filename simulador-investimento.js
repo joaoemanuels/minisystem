796fed836b023eac14b5a6412a571540e65aaba0
joaoemanuels <jemanuel.pi@gmail.com>
2025-11-01 20:22:54 -0300

    finalização

--- simulador-investimento.js
+++ simulador-investimento.js
@@ -1,12 +1,20 @@
+const simuladorInvestimento = (valor, taxaJuros = 0.1, meses = 12) => {
+  let saldo = valor;
 
-
-const simuladorInvestimento = (valor) => {
-  let valorInicial = 1000;
-  let taxaJuros = 0.1;
-  let valorFinal = 0;
-  let prazo = 12;
-
-  for (prazo = 0; prazo <= 12; prazo++) {
-    valorFinal += valorInicial + valorInicial * taxaJuros;
+  console.log(`evolução do saldo mês a mês é:`);
+  for (mes = 1; mes <= meses; mes++) {
+    saldo += saldo * taxaJuros;
+    console.log(`Mês ${mes}: R$${saldo.toFixed(2)}`);
   }
+
+  let lucro = saldo - valor;
+  let lucroPercentual = ((saldo - valor) / valor) * 100;
+
+  console.log(
+    `Sendo o valor final de R$${saldo.toFixed(2)} e lucro de R$${lucro.toFixed(
+      2
+    )}, um ganho de ${lucroPercentual.toFixed(1)}%`
+  );
 };
+
+simuladorInvestimento(1518);
